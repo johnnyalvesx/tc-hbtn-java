1041b95e346933c3e84e068f63e2da07fd3b1b88
johnnyalvesx <alvesjohnny@outlook.com>
2022-01-26 18:26:56 -0300

    Contador de pares 2

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -1,14 +1,12 @@
-package numeros;
-
 public class NumerosPares {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i <= 100; i++) {
+		for (int i = 0; i < 100; i++) {
 			if (i % 2 == 0) {
-				if (i < 100) {
+				if (i < 98) {
 					System.out.print(i + ", ");
-				} else if (i == 100) {
+				} else if (i == 98) {
 					System.out.print(i + "\n");
 				}
 			}
