34101fef59d041e4d7c474004060644077a5d4a9
johnnyalvesx <alvesjohnny@outlook.com>
2022-01-26 15:14:34 -0300

    Correção dos decimais

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,12 +1,20 @@
+package java_study;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 public class PrintFormatacaoNumeros {
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
 		float taxa = 0.2456f;
 		float valor = 7654.321f;
-		System.out.printf("Valor: R$ %.2f\nTaxa: %.2f", valor, taxa);
-		System.out.println("%");
+		DecimalFormat df = new DecimalFormat("0,000.00");
+		df.setRoundingMode(RoundingMode.HALF_EVEN);
+		NumberFormat nf = NumberFormat.getInstance(new Locale("en", "US"));
+		String val = nf.format(valor);
+		System.out.printf("Valor: R$ ");
+		System.out.println(df.format(valor));
+		System.out.printf("Taxa: %.2f%%\n", taxa);
 	}
 }
