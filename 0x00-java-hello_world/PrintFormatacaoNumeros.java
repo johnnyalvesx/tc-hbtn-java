176a9347bfc71708e3396ce14ac56dcd799a4caf
johnnyalvesx <alvesjohnny@outlook.com>
2022-01-26 14:09:24 -0300

    Código reformatado

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,7 +1,12 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class PrintFormatacaoNumeros {
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		float taxa = 0.2456f;
-        float valor = 7654.321f;
-        System.out.printf("Valor: R$ %.3f\nTaxa: R$ %.2f\n", valor, taxa);
+		float valor = 7654.321f;
+		System.out.printf("Valor: R$ %.2f\nTaxa: %.2f", valor, taxa);
+		System.out.println("%");
 	}
 }
