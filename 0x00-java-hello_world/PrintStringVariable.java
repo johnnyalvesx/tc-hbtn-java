f3feb4c4b351f840e26b291d733b6433ae2440b8
johnnyalvesx <alvesjohnny@outlook.com>
2022-01-26 12:29:49 -0300

    Imprimir String e substring

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintStringVariable {
+	public static void main(String[] args) {
+		String texto = "Holberton School\nHolberton School\n";
+		System.out.println(texto + texto.substring(0,9));
+	}
+}
