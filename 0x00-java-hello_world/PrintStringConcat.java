31f7921f3024d1850161bbac2cb51edcc7624f33
johnnyalvesx <alvesjohnny@outlook.com>
2022-01-26 13:21:04 -0300

    Novo teste 3

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,8 +1,8 @@
 public class PrintStringConcat {
 	public static void main(String[] args) {
+		String texto0 = "Bem vindo a ";
 		String texto1 = "Holberton ";
-		String texto2 = "School";
-		System.out.println("Bem vindo a " + texto1 
-				+ texto2 + "!");
+		String texto2 = "School!";
+		System.out.println(texto0 + texto1 + texto2);
 	}
 }
