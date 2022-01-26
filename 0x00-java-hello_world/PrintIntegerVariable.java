f650974d64b3f00ae17b6a914a48cb75233bfbff
johnnyalvesx <alvesjohnny@outlook.com>
2022-01-26 11:34:11 -0300

    Imprimir String com int

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,12 @@
+package java_study;
+
+public class PrintIntegerVariable {
+
+	public static void main(String[] args) {
+		
+		int numero = 321;
+		
+		System.out.println("Avenida Andaló, " + numero);
+	}
+
+}
