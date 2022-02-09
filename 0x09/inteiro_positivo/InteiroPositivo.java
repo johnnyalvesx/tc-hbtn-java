30cd7d0105e9a79ca17564c80c45d92b6f27fdac
johnnyalvesx <alvesjohnny@outlook.com>
2022-02-09 18:15:08 -0300

    Boxing/unboxing

---
+++ 0x09/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,42 @@
+package inteiro_positivo;
+
+public class InteiroPositivo {
+
+	private int valor;
+	private int result;
+
+	public InteiroPositivo(int valor) {
+		this.valor = valor;
+	}
+
+	public InteiroPositivo(String valor) {
+		this.valor = Integer.valueOf(valor);
+		setValor(this.valor);
+	}
+
+	public boolean ehPrimo() {
+		for (int i = 1; i <= valor; i++) {
+			if (valor % i == 0) {
+				result++;
+			}
+		}
+
+		if (result == 2) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		if (valor > 0) {
+			this.valor = valor;
+		} else {
+			throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+		}
+	}
+}
