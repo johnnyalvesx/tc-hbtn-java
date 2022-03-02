6852efa13a62c08956e6d8e577b596c47d5e708d
johnnyalvesx <alvesjohnny@outlook.com>
2022-03-02 12:01:07 -0300

    Collectors - Task 1

---
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import pessoa_por_cargo.Pessoa;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+
+	return pessoas.stream().collect(Collectors.groupingBy(e -> e.getCargo()));
+
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+
+	return pessoas.stream().collect(
+		Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+
+    }
+}
