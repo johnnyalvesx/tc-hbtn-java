77890b7efa8cf2079464f5469472718ebcdff514
johnnyalvesx <alvesjohnny@outlook.com>
2022-03-08 09:42:23 -0300

    Generics - Exercícios - Task 1

---
+++ 0x22/armazem_especifico/Armazem.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Armazem<T> implements Armazenavel<T> {
+
+    public Map<String, T> itens = new HashMap<>();
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+	itens.put(nome, valor);
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+	return itens.get(nome);
+    }
+}
---
+++ 0x22/armazem_especifico/Armazenavel.java
@@ -0,0 +1,7 @@
+interface Armazenavel<T> {
+
+    void adicionarAoInventario(String nome, T valor);
+
+    T obterDoInventario(String nome);
+
+}
