fc586eec3bc30358cd5dd6458b6c22edaa26920f
johnnyalvesx <alvesjohnny@outlook.com>
2022-03-10 10:22:29 -0300

    Correção 4

--- 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
+++ 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
@@ -1,22 +1,18 @@
 public class Aritmeticos {
 
     public static int somar(int valor1, int valor2){
-      int soma =  valor1 + valor2;
-      return soma;
+        return valor1 + valor2;
     }
 
     public static int subtrair(int valor1, int valor2){
-        int subtracao = valor1 - valor2;
-        return  subtracao;
+        return valor1 - valor2;
     }
 
     public static int multiplicar(int valor1, int valor2){
-        int multiplicacao = valor1 * valor2;
-        return multiplicacao;
+        return valor1 * valor2;
     }
 
     public static int dividir(int valor1, int valor2){
-        int dividir = valor1/valor2;
-        return dividir;
+        return valor1 / valor2;
     }
 }
