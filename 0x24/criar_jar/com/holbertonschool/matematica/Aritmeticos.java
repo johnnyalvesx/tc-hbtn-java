b9db3c62a508693dab43f5ca4ab593778f6eac63
johnnyalvesx <alvesjohnny@outlook.com>
2022-03-10 09:57:43 -0300

    Pacotes JAR - Task 0

--- 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
+++ 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
@@ -1,16 +1,22 @@
 public class Aritmeticos {
-	
-	public static int somar(int valor1, int valor2) {
-		return valor1+valor2;
-	}
-	public static int subtrair(int valor1, int valor2) {
-		return valor1-valor2;
-	}
-	public static int multiplicar(int valor1, int valor2) {
-		return valor1*valor2;
-	}
-	public static int dividir(int valor1, int valor2) {
-		return valor1/valor2;
-	}
 
+    public static int somar(int valor1, int valor2){
+      int soma =  valor1 + valor2;
+      return soma;
+    }
+
+    public static int subtrair(int valor1, int valor2){
+        int subtracao = valor1 - valor2;
+        return  subtracao;
+    }
+
+    public static int multiplicar(int valor1, int valor2){
+        int multiplicacao = valor1 * valor2;
+        return multiplicacao;
+    }
+
+    public static int dividir(int valor1, int valor2){
+        int dividir = valor1/valor2;
+        return dividir;
+    }
 }
